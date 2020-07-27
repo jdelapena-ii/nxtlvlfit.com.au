6155deeea35d2d20f65c2661b92e7df78022eff0
Luke Bennett <hello@lukebennett.com.au>
2020-07-27 17:29:58 +1000

    Update page title

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,7 +5,7 @@ import { Layout, SEO } from '../components';
 function SuccessPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
+      <SEO title="Message received" />
       <article className="w-full max-w-md mx-auto mt-24 sm:mt-40">
         <div className="px-4 space-y-8 tracking-widest text-center uppercase">
           <h2 className="relative text-2xl font-semibold leading-none tracking-widest text-center uppercase sm:text-4xl">
