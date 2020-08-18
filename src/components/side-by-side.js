97ebd275035017f3abb6ac35dfadcdb442781818
Luke Bennett <hello@lukebennett.com.au>
2020-08-18 12:57:41 +1000

    Remove unneeded keys

--- src/components/side-by-side.js
+++ src/components/side-by-side.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 
 function SideBySide({ heading, children, cta, component, reverse }) {
   function Copy() {
     return (
       <div className="flex flex-col justify-center max-w-lg space-y-8">
         <h2>{heading}</h2>
-        <div key={nanoid()} className="prose">
-          {children}
-        </div>
-        {cta && (
-          <div className="flex flex-wrap items-center -mt-4" key={nanoid()}>
-            {cta}
-          </div>
-        )}
+        <div className="prose">{children}</div>
+        {cta && <div className="flex flex-wrap items-center -mt-4">{cta}</div>}
       </div>
     );
   }
@@ -24,7 +17,7 @@ function SideBySide({ heading, children, cta, component, reverse }) {
   return (
     <article className="relative grid gap-12 px-4 md:grid-cols-2 sm:px-6 lg:px-8">
       {reverse && <SecondaryComponent />}
-      <Copy key={nanoid()} />
+      <Copy />
       {!reverse && <SecondaryComponent />}
     </article>
   );
