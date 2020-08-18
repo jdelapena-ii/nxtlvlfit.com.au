00a90077598ae95d383d7373a2bc2af0a478ecbe
Luke Bennett <hello@lukebennett.com.au>
2020-08-18 12:55:52 +1000

    Enable image and remove unsued props

--- src/components/text-with-image.js
+++ src/components/text-with-image.js
@@ -1,35 +1,23 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import GatsbyImage from 'gatsby-image';
-import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 
-import { AttentionBar } from './attention-bar';
 import { SideBySide } from './side-by-side';
 
-function TextWithImage({
-  children,
-  cta,
-  heading,
-  image,
-  paddingTop,
-  reverse,
-  showAttentionBar,
-}) {
-  // function Image() {
-  //   return (
-  //     <div className="relative order-1 w-full md:order-none">
-  //       <GatsbyImage fluid={image} className="w-full h-full rounded-xl" />
-  //       {showAttentionBar && <AttentionBar />}
-  //     </div>
-  //   );
-  // }
+function TextWithImage({ children, cta, heading, image, reverse }) {
+  function Image() {
+    return (
+      <div className="relative order-1 w-full md:order-none">
+        <GatsbyImage fluid={image} className="w-full h-full" />
+      </div>
+    );
+  }
 
   return (
     <SideBySide
+      component={<Image />}
       heading={heading}
       cta={cta}
-      // component={<Image key={nanoid()} />}
-      paddingTop={paddingTop}
       reverse={reverse}
     >
       {children}
@@ -42,9 +30,7 @@ TextWithImage.propTypes = {
   cta: PropTypes.node,
   heading: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   image: PropTypes.object.isRequired,
-  paddingTop: PropTypes.string,
   reverse: PropTypes.bool,
-  showAttentionBar: PropTypes.bool,
 };
 
 export { TextWithImage };
