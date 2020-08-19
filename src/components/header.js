3f8c1276faa055dfed0996482be1040a26359cf2
Jedo <jtdelapenaii@gmail.com>
2020-08-19 11:08:49 +0800

    added spacing in menus

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+
 export function Header() {
     return (
-        <header className="relative">
+        <header className="relative flex content-center">
             <nav> 
-                <ul>
-                    <li className="mt-8">
+                <ul className="flex items-center tracking-widest uppercase">
+                    <li className="mt-12 mr-24">
+                        <Link to="/">nXtLvL</Link>
+                    </li>
+                    <li className="mt-12 mr-24">
                         <Link to="/meet-us">Meet Us</Link>
                     </li>
-                    <li className="mt-8">
+                    <li className="mt-12 mr-24">
                         <Link to="/membership">Membership</Link>
                     </li>
-                    <li className="mt-8">
+                    <li className="mt-12 mr-24">
                         <Link to="/timetable">Timetable</Link>
                     </li>
-                    <li className="mt-8">
+                    <li className="mt-12 mr-24">
                         <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
