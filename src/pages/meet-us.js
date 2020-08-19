9693f667eb0427954fb5b5ef3846c37c0938666e
Jedo <jtdelapenaii@gmail.com>
2020-08-19 11:23:04 +0800

    set up meet us page

--- src/pages/meet-us.js
+++ src/pages/meet-us.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-import { Layout, SEO } from '../components';
+import { Layout, SEO, TextWithImage } from '../components';
 
 export default function MeetUsPage() {
   return (
     <Layout isHome>
       <SEO title="Coming soon" />
+      <FirstImage />
+      <SecondImage />
     </Layout>
   );
+}
+
+
+function FirstImage() {
+  const { file } = useStaticQuery(graphql`
+  {
+    file(relativePath: { eq: "background-image.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`);
+
+  return (
+    <TextWithImage  
+      heading="How It Works"
+      image={file.childImageSharp.fluid}
+      reverse
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
+        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
+        in vel doloribus earum!
+      </p>
+    </TextWithImage>
+  )
 }
