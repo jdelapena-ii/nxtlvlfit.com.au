d6fe6906475771601b49dbbe96bd657c211a49aa
Luke Bennett <hello@lukebennett.com.au>
2020-08-23 22:20:24 +1000

    Refactor footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,61 +1,96 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-import { Link } from 'gatsby'
+import { mainNavigation, socialLinks } from '../data';
+import { useGraphQL } from '../hooks';
 
 export function Footer() {
+  const {
+    site: { siteMetadata },
+  } = useGraphQL();
   return (
-    <footer className="relative">
-      <nav>
-        <ul className="flex items-center justify-center tracking-widest uppercase"> 
-          <li className="mt-12 mr-24">Links
-            <Links />
-          </li>
-          <li className="mt-12 mr-24">
-            <Contact />
-          </li>
-          <li className="mt-12 mr-24">Join us</li>
-        </ul>
-      </nav>
-      <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="mt-8">
-          <p className="text-center uppercase">
-            <a
-              href="https://www.phirannodesigns.com.au/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Website by Phiranno Designs
-            </a>
-          </p>
-        </div>
+    <footer className="relative px-4 py-12 bg-black sm:px-6 lg:px-8">
+      <div className="grid w-full max-w-3xl grid-cols-3 gap-4 mx-auto uppercase">
+        <nav className="space-y-2">
+          <h2 className="text-xl font-bold tracking-widest uppercase">Links</h2>
+          <ul className="space-y-2">
+            {mainNavigation.map((navItem) =>
+              navItem.submenu ? null : (
+                <li key={navItem.id}>
+                  <Link to={navItem.slug}>{navItem.label}</Link>
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
+        <nav className="space-y-2">
+          <h2 className="text-xl font-bold tracking-widest uppercase">
+            Contact
+          </h2>
+          <ul className="space-y-2 normal-case">
+            <li>
+              <dl>
+                <dt className="inline">Rich: </dt>
+                <dd className="inline">{siteMetadata.phone.rich}</dd>
+              </dl>
+            </li>
+            <li>
+              <dl>
+                <dt className="inline">Kez: </dt>
+                <dd className="inline">{siteMetadata.phone.kez}</dd>
+              </dl>
+            </li>
+            <li>
+              <dl>
+                <dt className="inline">Address: </dt>
+                <dd className="inline">
+                  {siteMetadata.address.street} <br />
+                  {siteMetadata.address.suburb} {siteMetadata.address.state}
+                </dd>
+              </dl>
+            </li>
+            <li>
+              <dl>
+                <dt className="inline">Email: </dt>
+                <dd className="inline">{siteMetadata.email}</dd>
+              </dl>
+            </li>
+          </ul>
+          <ul className="flex space-x-4 text-xl tracking-widest uppercase">
+            {socialLinks.map((link) => (
+              <li key={link.id}>
+                <a href={link.url} className="inline-block rounded-full">
+                  <span className="sr-only">{link.label}</span>
+                  <link.icon />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <nav className="space-y-2">
+          <h2 className="text-xl font-bold tracking-widest uppercase">
+            Join Us
+          </h2>
+          <button
+            type="button"
+            className="w-full px-8 py-2 text-sm tracking-widest uppercase border-2 border-white"
+          >
+            Sign up
+          </button>
+        </nav>
+      </div>
+
+      <div className="mt-8">
+        <p className="text-center uppercase">
+          <a
+            href="https://www.phirannodesigns.com.au/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Website by Phiranno Designs
+          </a>
+        </p>
       </div>
     </footer>
   );
-}
-
-
-function Links(){
-  return (
-    <div>
-      <ul>
-        <li><Link to="/membership">Membership</Link></li>
-        <li><Link to="/meet-us">Meet us</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/classes">News</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </div>
-  )
-}
-
-function Contact() {
-  return (
-    <div>
-      <h1>Contact</h1>
-      <p>Rich 0422 767 889</p>
-      <p>Kez 0402 322 131</p>
-      <p>Address:</p>
-      <p>Email:</p>
-    </div>
-  )
 }
