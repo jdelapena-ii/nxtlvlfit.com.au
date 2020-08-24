cf699e64fef17cb720038f54994b5e1faca24083
Luke Bennett <hello@lukebennett.com.au>
2020-08-24 18:02:00 +1000

    Refactor layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,17 +10,19 @@ import PropTypes from 'prop-types';
 
 import { Background } from './background';
 import { Hero } from './hero';
-import { Header } from './header';
+import { HeroNav } from './hero-nav';
 import { Footer } from './footer';
 
 export function Layout({ children, isHome }) {
   return (
     <div className="font-sans antialiased text-white bg-black">
-      <Background isHome={isHome} />
       <div className="flex flex-col w-full min-h-screen mx-auto max-w-7xl">
-        <Header />
-        <Hero isHome={isHome} />
-        <main className="relative flex flex-1">{children}</main>
+        <Background isHome={isHome} />
+        <HeroNav />
+        {isHome && <Hero />}
+        <main className="relative flex flex-col justify-end flex-1">
+          {children}
+        </main>
         <Footer />
       </div>
     </div>
