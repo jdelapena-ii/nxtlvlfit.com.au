55ab9a39f340d42d45e132dc6961cc9fe33954ff
Luke Bennett <hello@lukebennett.com.au>
2020-08-24 18:03:17 +1000

    Save progress on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 
-import { Layout, SEO, ContactForm } from '../components';
+import { Layout, SEO, Background, NavBar, Hero } from '../components';
+import { Welcome, ServicesGrid } from '../components/home';
 
-export default function IndexPage() {
+function IndexPage() {
   return (
-    <Layout isHome>
-      <SEO title="Coming soon" />
-      <ContactForm />
+    <Layout>
+      <SEO title="Home" />
+      <HeroSection />
+      <MainSection />
     </Layout>
   );
 }
+
+function HeroSection() {
+  return (
+    <div className="absolute inset-0">
+      <Background isHome />
+      <Hero />
+    </div>
+  );
+}
+
+function MainSection() {
+  return (
+    <div className="relative mt-12 pointer-events-none">
+      <Spacer />
+      <div className="pointer-events-auto">
+        <NavBar />
+        <Welcome />
+        <ServicesGrid />
+      </div>
+    </div>
+  );
+}
+
+function Spacer() {
+  return (
+    <div
+      aria-hidden
+      className="relative h-0 pointer-events-none aspect-ratio-16/9"
+    >
+      <div aria-hidden className="absolute inset-0" />
+    </div>
+  );
+}
+
+export default IndexPage;
