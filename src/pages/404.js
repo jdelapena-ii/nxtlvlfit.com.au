33799f6368430acab91e694cf1fea3a6fe5145c1
Luke Bennett <hello@lukebennett.com.au>
2020-08-25 09:25:00 +1000

    Styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,18 +6,23 @@ import { Layout, SEO } from '../components';
 function NotFoundPage() {
   return (
     <Layout isHome>
-      <SEO title="404: Not found" />
-      <article className="w-full max-w-2xl mx-auto mt-24 sm:mt-40">
-        <div className="px-32 py-16 space-y-8 tracking-widest text-center uppercase bg-black bg-opacity-75 border-4 border-white">
-          <h2 className="relative text-2xl font-bold leading-none tracking-widest text-center uppercase sm:text-4xl">
+      <SEO title="404: Page Not Found" />
+      <article className="relative flex items-center justify-center w-full px-4 my-24 sm:mt-40 sm:px-6 lg:px-8">
+        <div className="w-full max-w-2xl px-4 py-12 mx-auto space-y-8 tracking-widest text-center uppercase border-2 border-white sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-bold leading-none tracking-widest text-center uppercase sm:text-4xl">
             404: Page Not Found
           </h2>
-          <p className="text-gray-200">Sorry, the page you were looking
-           <br />
-           for could not be found</p>
-          <p className="px-0 py-4 border-4 border-white max-2-md">
-            <Link to="/" className="hover:underline">
-              &larr; Return Home
+          <p className="text-gray-200">
+            Sorry, the page you were looking for
+            <br />
+            could not be found
+          </p>
+          <p>
+            <Link
+              to="/"
+              className="inline-block w-full max-w-xs px-4 py-4 mx-auto transition duration-300 ease-in-out border-2 border-white hover:bg-white hover:text-black"
+            >
+              Return Home
             </Link>
           </p>
         </div>
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
 
 import { Layout, SEO } from '../components';
 
@@ -7,16 +7,22 @@ function SuccessPage() {
   return (
     <Layout isHome>
       <SEO title="Message received" />
-      <article className="w-full max-w-2xl mx-auto mt-24 sm:mt-40">
-        <div className="px-32 py-16 space-y-8 tracking-widest text-center uppercase bg-black bg-opacity-75 border-4 border-white">
-          <h2 className="relative text-2xl font-bold leading-none tracking-widest text-center uppercase sm:text-4xl">SUCCESS</h2>
-          <br />
-          <p className="text-gray-200">Thank you, our team will get
-          <br /> 
-          back to you shortly</p>
-          <p className="max-w-md px-0 py-4 border-4 border-white">
-            <Link to="/" className="hover:underline">
-              &larr; Return Home
+      <article className="relative flex items-center justify-center w-full px-4 my-24 sm:mt-40 sm:px-6 lg:px-8">
+        <div className="w-full max-w-2xl px-4 py-12 mx-auto space-y-8 tracking-widest text-center uppercase border-2 border-white sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-bold leading-none tracking-widest text-center uppercase sm:text-4xl">
+            SUCCESS
+          </h2>
+          <p className="text-gray-200">
+            Thank you, our team will get
+            <br />
+            back to you shortly
+          </p>
+          <p>
+            <Link
+              to="/"
+              className="inline-block w-full max-w-xs px-4 py-4 mx-auto transition duration-300 ease-in-out border-2 border-white hover:bg-white hover:text-black"
+            >
+              Return Home
             </Link>
           </p>
         </div>
