4d4bd90d9fe25bef885c12c93bdc1c8a0d0f099f
Luke Bennett <hello@lukebennett.com.au>
2020-08-25 15:32:22 +1000

    Formatting

--- src/components/map.js
+++ src/components/map.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import {
-    GoogleMap,
-    useLoadScript,
-    Marker,
-    InfoWindow,
-  } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
-  const libraries = ["places"];
+const libraries = ['places'];
 
-  const mapContainerStyle = {
-    height: "70vh",
-    width: "84.5vw",
-  };
+const mapContainerStyle = {
+  height: '100%',
+  width: '100%',
+};
 
-  const center = {
-    lat: -31.433332,
-    lng: 152.899994,
-  };
+const center = {
+  lat: -31.433332,
+  lng: 152.899994,
+};
 
 function Map() {
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.GATSBY_GOOGLE_MAPS_API_KEY,
-        libraries,
-    });
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.GATSBY_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
 
-    if (loadError) return "Error";
-    if (!isLoaded) return "Loading...";
-    
-    return (
-        <div>
-           <GoogleMap
-              mapContainerStyle={mapContainerStyle}
+  if (loadError) return 'Error';
+  if (!isLoaded) return 'Loading...';
+
+  return (
+    <div className="relative h-0 aspect-ratio-16/9">
+      <div className="absolute inset-0">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
               zoom={8}
               center={center}
-           >
-           </GoogleMap>
-        </div>
-      )
-  }
+          center={center}
+        />
+      </div>
+    </div>
+  );
+}
 
   export default Map;
