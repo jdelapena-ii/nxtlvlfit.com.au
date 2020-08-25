6d1f11f7c90df8c7f482af2e3b3459be1240738b
NOP-slide <phelps.andrewj@gmail.com>
2020-08-25 15:32:25 +0930

    build blog posts from template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allSanityPost {
+        nodes {
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  // Create blog posts from Sanity
+  const posts = result.data.allSanityPost.nodes || [];
+  posts.forEach((post) => {
+    const {
+      slug: { current: slug },
+    } = post;
+    createPage({
+      path: `/posts/${slug}`,
+      component: require.resolve('./src/templates/post.js'),
+      context: { slug },
+    });
+  });
+};
