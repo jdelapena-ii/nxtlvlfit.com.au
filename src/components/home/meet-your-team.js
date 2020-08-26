da1d6a04318d1b60c52947cda1f02a9a8b0127c5
Luke Bennett <hello@lukebennett.com.au>
2020-08-26 16:50:53 +1000

    Style focus states

--- src/components/home/meet-your-team.js
+++ src/components/home/meet-your-team.js
@@ -13,7 +13,7 @@ function MeetYourTeam() {
       <div className="absolute inset-0 flex items-center justify-center">
         <Link
           to="/meet-us/"
-          className="block w-full max-w-xs px-4 py-4 mx-auto my-24 text-2xl font-bold leading-none tracking-widest text-center uppercase border-2 border-white text-shadow"
+          className="block w-full max-w-xs px-4 py-4 mx-auto my-24 text-2xl font-bold leading-none tracking-widest text-center uppercase transition duration-300 ease-in-out border-2 border-white text-shadow hover:bg-white hover:text-black hover:text-shadow-none focus:bg-white focus:text-black focus:text-shadow-none"
         >
           Meet your Team
         </Link>
