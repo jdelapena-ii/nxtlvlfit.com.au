d851c8fe31c1fac35a32b0aca03ec01a1a7c1391
Luke Bennett <hello@lukebennett.com.au>
2020-08-28 17:43:50 +1000

    Add team photos

--- src/pages/meet-us.js
+++ src/pages/meet-us.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
@@ -12,22 +13,9 @@ import {
   ContactPageForm,
   Map,
 } from '../components';
-import { useGraphQL } from '../hooks';
 
 function MeetUsPage() {
-  const { meetUsHeroImage } = useStaticQuery(
-    graphql`
-      {
-        meetUsHeroImage: file(relativePath: { eq: "meet-us-hero-image.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 1920) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `
-  );
+  const { meetUsHeroImage } = useImages();
   return (
     <Layout>
       <SEO title="Meet the nXtLvL Team" />
@@ -51,9 +39,11 @@ function MeetUsPage() {
               inventore tempora. Harum.
             </p>
           </Intro>
-          <MattMoore />
-          <AshPollard />
-          <KateBlush />
+          <Jodie />
+          <Nige />
+          <Harry />
+          <Rich />
+          <Kez />
         </div>
       </GradientContainer>
       <Instagram />
@@ -63,81 +53,149 @@ function MeetUsPage() {
   );
 }
 
-function MattMoore() {
-  const { hayleyKim } = useGraphQL();
+function Jodie() {
+  const { jodieLea } = useImages();
   return (
     <TeamMember
-      eyebrowText="Coach/Crossfit Trainer"
-      heading="Matt Moore"
-      image={hayleyKim.childImageSharp.fluid}
+      // eyebrowText="Coach/Crossfit Trainer"
+      heading="Jodie"
+      image={jodieLea.childImageSharp.fluid}
       aspectRatio="aspect-ratio-3/4"
       reverse
     >
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
+      <ul>
+        <li>AWF 1 &amp; 2 Lifting</li>
+        <li>CrossFit Level 1 &amp; 2</li>
+        <li>Functional Fitness</li>
+        <li>Rowing</li>
+      </ul>
     </TeamMember>
   );
 }
 
-function AshPollard() {
-  const { svenMieke } = useGraphQL();
+function Nige() {
+  const { nige } = useImages();
   return (
     <TeamMember
-      eyebrowText="Personal/Crossfit Trainer"
-      heading="Ash Pollard"
-      image={svenMieke.childImageSharp.fluid}
+      // eyebrowText="Personal/Crossfit Trainer"
+      heading="Nige"
+      image={nige.childImageSharp.fluid}
       aspectRatio="aspect-ratio-3/4"
     >
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
+      <ul>
+        <li>Functional Fitness</li>
+        <li>CrossFit Level 1</li>
+        <li>Power Lifting</li>
+        <li>Technique &amp; Corrective Exercises</li>
+      </ul>
     </TeamMember>
   );
 }
 
-function KateBlush() {
-  const { hayleyKim } = useGraphQL();
+function Harry() {
+  const { harry } = useImages();
   return (
     <TeamMember
-      eyebrowText="Power Lift Trainer"
-      heading="Kate Blush"
-      image={hayleyKim.childImageSharp.fluid}
+      // eyebrowText="Power Lift Trainer"
+      heading="Harry"
+      image={harry.childImageSharp.fluid}
       aspectRatio="aspect-ratio-3/4"
       reverse
     >
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-        architecto modi, ratione blanditiis quis facere nulla repudiandae, quia
-        perspiciatis quae aspernatur iusto laudantium itaque nesciunt molestias,
-        in vel doloribus earum!
-      </p>
+      <ul>
+        <li>Calisthenics</li>
+      </ul>
     </TeamMember>
   );
+}
+
+function Rich() {
+  const { rich } = useImages();
+  return (
+    <TeamMember
+      // eyebrowText="Power Lift Trainer"
+      heading="Rich"
+      image={rich.childImageSharp.fluid}
+      aspectRatio="aspect-ratio-3/4"
+    >
+      <ul>
+        <li>Functional Fitness</li>
+        <li>AWF Level 1</li>
+        <li>CrossFit Level 1</li>
+      </ul>
+    </TeamMember>
+  );
+}
+
+function Kez() {
+  const { kez } = useImages();
+  return (
+    <TeamMember
+      // eyebrowText="Power Lift Trainer"
+      heading="Kez"
+      image={kez.childImageSharp.fluid}
+      aspectRatio="aspect-ratio-3/4"
+      reverse
+    >
+      <ul>
+        <li>Functional Fitness</li>
+        <li>AWF Level 1</li>
+        <li>CrossFit Level 1</li>
+      </ul>
+    </TeamMember>
+  );
+}
+
+export function useImages() {
+  const data = useStaticQuery(
+    graphql`
+      {
+        harry: file(relativePath: { eq: "harry.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        jodieLea: file(relativePath: { eq: "jodie-lea.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        kez: file(relativePath: { eq: "kez.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        meetUsHeroImage: file(relativePath: { eq: "meet-us-hero-image.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        nige: file(relativePath: { eq: "nige.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        rich: file(relativePath: { eq: "rich.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  );
+  return data;
 }
 
 export default MeetUsPage;
