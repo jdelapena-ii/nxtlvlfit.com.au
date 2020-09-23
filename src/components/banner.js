eb14e3596c52bf3b973d23419b8cbb888b8951ad
Luke Bennett <hello@lukebennett.com.au>
2020-09-23 16:02:07 +1000

    Add opening day

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Banner() {
   return (
     <article className="pb-1.5 z-10 pt-2 text-sm font-black leading-none tracking-ultra-wide text-center uppercase bg-red-700 sticky top-0 inset-x-0 h-8 flex items-center w-full justify-center">
-      Grand opening October 2020
+      Grand Opening 17<sup className="lowercase">th</sup>&nbsp;October 2020
     </article>
   );
 }
