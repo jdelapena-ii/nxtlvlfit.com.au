553dcd08d763d7c2ad33f8d2a9701c91114db32f
NOP-slide <phelps.andrewj@gmail.com>
2020-09-29 09:28:11 +0930

    add Google tag manager code

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,6 +18,13 @@ function Layout({ children, isHome }) {
   return (
     <>
       <Helmet>
+        {/* Google tag manager  */}
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6RPBV6');`}</script>
+
         {/* google conversion tracking */}
         <script
           async
@@ -31,6 +38,15 @@ function Layout({ children, isHome }) {
   gtag('config', 'AW-591313295');`}
         </script>
       </Helmet>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-P6RPBV6"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+
       <div className="font-sans antialiased text-white bg-black fill-available">
         <div className="flex flex-col w-full min-h-screen mx-auto max-w-7xl">
           <div className="relative flex flex-col flex-1">
