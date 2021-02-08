7b57ebbe6e29c03799335841b2d0dd857f55aade
Jedo <jtdelapenaii@gmail.com>
2021-02-08 17:36:27 +0800

    changed success page title

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,7 @@ import { Layout, SEO } from '../components';
 function SuccessPage() {
   return (
     <Layout isHome>
-      <SEO title="Message received" />
+      <SEO title="Success" />
       <article className="relative flex items-center justify-center w-full px-4 my-24 sm:mt-40 sm:px-6 lg:px-8">
         <div className="w-full max-w-2xl px-4 py-12 mx-auto space-y-8 tracking-widest text-center uppercase border-2 border-white sm:px-6 lg:px-8">
           <h2 className="text-2xl font-bold leading-none tracking-widest text-center uppercase sm:text-4xl">
